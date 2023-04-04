c622886ade9d1e706296774298855d4800637772
tysunday <120739860+tysunday@users.noreply.github.com>
2023-04-04 21:33:30 +0500

    Add files via upload

--- cw_03.04.2023_elevator/Elevator.h
+++ cw_03.04.2023_elevator/Elevator.h
@@ -1,7 +1,6 @@
 #pragma once
 #include <iostream>
 using namespace std;
-
 class Elevator
 {
 public:
@@ -9,23 +8,29 @@ public:
 	{
 		cout << "Сейчас вы на этаже:" << currentFloors << endl;
 		cout << "На какой вам этаж? \n";
+		cout << "Всего этажей : \n";
 		for (int i = floors[0]; i < floors[11]; i++) {
-			cout << "Этаж:" << "\n";
+			cout << "Этаж: " << i << "\n";
 		}
 		cin >> desiredFloor;
-		while (desiredFloor > 10 && desiredFloor < -1)
+		system("cls");
+		while (desiredFloor > 9 || desiredFloor < -1)
 		{
-			cout << "Всего этажей : \n";
+			cout << "Сейчас вы на этаже:" << currentFloors << endl;
 			cout << "Такого этажа нет, попробуйте ещё раз:\n";
+			cout << "Всего этажей : \n";
+			for (int i = floors[0]; i < floors[11]; i++) {
+				cout << "Этаж: " << i << "\n";
+			}
 			cin >> desiredFloor;
+			system("cls");
 		}
 		currentFloors = desiredFloor;
 		return currentFloors;
 	}
 private:
-
 	int floors[12]{ -1,0,1,2,3,4,5,6,7,8,9,10 }; // этажи
-	int currentFloors; // текущий этаж
+	int currentFloors = 0; // текущий этаж
 	int desiredFloor; // желаемый этаж
 	bool togle; // вкл выкл?
 };
