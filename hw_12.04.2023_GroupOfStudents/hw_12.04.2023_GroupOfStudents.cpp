56f45c65c20b0965369b6627e959d5f898db1aca
tysunday <120739860+tysunday@users.noreply.github.com>
2023-04-13 00:01:25 +0500

    Add files via upload

---
+++ hw_12.04.2023_GroupOfStudents/hw_12.04.2023_GroupOfStudents.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+
+
+using namespace std;
+
+
+class Lesson
+{
+public:
+	Lesson(string _title, int _countGrade, int _averageGrade, int _grades[]) : title(_title), countGrade(_countGrade) {}; // member initializer list
+	
+
+	void CreateLesson(int _countGrade)
+	{
+		countGrade = _countGrade;
+		lesson = new Lesson* [countGrade];
+
+		for (int i = 0; i < countGrade; i++)
+		{
+			string title;
+			int countGrade = 0, averageGrade = 0;
+			int* grades = new int[countGrade];
+			cout << "Введите название предмета.\n";
+			cin >> title;
+
+		}
+
+	}
+
+private:
+	Lesson** lesson; // массив с оценками.
+	string title; // название предмета.
+	int countGrade; // количество оценок.
+	int averageGrade; // средняя оценка.
+};
+
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	srand(time(NULL));
+
+	int countGrade = 5 + rand() % 10;
+
+}
+
