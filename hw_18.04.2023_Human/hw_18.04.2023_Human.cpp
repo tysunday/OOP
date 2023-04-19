6267432fc798ad5a65ea7ef5eaecc8961fa1c56d
tysunday <120739860+tysunday@users.noreply.github.com>
2023-04-19 20:57:05 +0500

    Add files via upload

--- hw_18.04.2023_Human/hw_18.04.2023_Human.cpp
+++ hw_18.04.2023_Human/hw_18.04.2023_Human.cpp
@@ -8,63 +8,71 @@ public:
 
 	Date() {}
 
-	void SetDate() {
+	void SetDate(int i) {
 		int year, month, day; bool first = true;
+		_count = i;
+		_dateOfBirth = new Date * [_count];
 
-		cout << "Введите год рождения: "; cin >> year;
-		do {
-			if (first) {
-				cout << "Введите месяц рождения: ";
-				cin >> month;
-				first = false;
-			}
-			else {
-				cout << "Месяцев всего 12, введите число от 1 до 12: ";
-				cin >> month;
-				
-			}
-		} while (month > 12 || month < 1);
-		first = true;
-		cout << "Введите день рождения: "; cin >> day;
-		dateOfBirth = new Date(day, month, year);
+		cout << "Введите дату рождения (ДД.ММ.ГГГГ) " << i + 1 << "-го человека: ";
+		scanf_s("%d.%d.%d", &day, &month, &year);
+
+		_dateOfBirth[i] = new Date(day, month, year);
 	}
 
 	void showDate() {
-		cout << dateOfBirth->_day << endl;
-		cout << dateOfBirth->_month << endl;
-		cout << dateOfBirth->_year << endl;
+		int i = 0;
+		cout << "О дате рождения какого объекта класса человек вы хотите узнать информацию? Введите число от 1 до " << _count << endl;
+		cin >> i;
+		cout << "Дата рождения (ДД.ММ.ГГГГ): " << _dateOfBirth[i]->_day << ".";
+		cout << _dateOfBirth[i]->_month << ".";
+		cout << _dateOfBirth[i]->_year << endl;
 	}
 
 private:
 	int _day;
 	int _month;
 	int _year;
-	Date* dateOfBirth;
+	Date** _dateOfBirth;
+	int _count;
 };
 
 class Human : Date {
 public:
 
 	Human(int idHuman, string firstName, string secondName, string thirdName, Date dateOfBirth) :
-		_idHuman(idHuman), _firstName(firstName), _secondName(secondName), _thirdName(thirdName), _dateOfBirth(dateOfBirth) {}
+		_idHuman(idHuman), _firstName(firstName), _secondName(secondName), _thirdName(thirdName), Date(dateOfBirth) {}
 
-	Human() {
-		int idHuman;
-		string firstName, secondName, thirdName;
-		Date dateOfBirth;
-		cout << "Введите id человека: "; cin >> idHuman;
-		cout << "Введите имя: "; cin >> firstName;
-		cout << "Введите фамилию: "; cin >> secondName;
-		cout << "Введите отчество: "; cin >> thirdName;
-		SetDate();
-		human = new Human(idHuman, firstName, secondName, thirdName, dateOfBirth);
+	int GetCount() { return _count; }
+
+	void CreateHuman() {
+		cout << "Сколько объектов класса человек вы хотите создать? ";
+		cin >> _count;
+		human = new Human * [_count];
+
+		for (int i = 1; i <= _count; i++)
+		{
+			int idHuman;
+			string firstName, secondName, thirdName;
+			Date dateOfBirth;
+			cout << "Введите id человека: "; cin >> idHuman;
+			cout << "Введите имя: "; cin >> firstName;
+			cout << "Введите фамилию: "; cin >> secondName;
+			cout << "Введите отчество: "; cin >> thirdName;
+			SetDate(i);
+			human[i] = new Human(idHuman, firstName, secondName, thirdName, dateOfBirth);
+		}
 	}
 
+	Human() {}
+
 	void ShowHumanInfo() {
-		cout << human->_idHuman << endl;
-		cout << human->_firstName << endl;
-		cout << human->_secondName << endl;
-		cout << human->_thirdName << endl;
+		int i = 0;
+		cout << "О каком объекте класса человек вы хотите узнать информацию? Введите число от 1 до " << _count << endl;
+		cin >> i;
+		cout << "id: " << human[i]->_idHuman << endl;
+		cout << "Имя: " << human[i]->_firstName << endl;
+		cout << "Фамилия: " << human[i]->_secondName << endl;
+		cout << "Отчество: " << human[i]->_thirdName << endl;
 		showDate();
 	}
 
@@ -78,10 +86,10 @@ private:
 	string _firstName;
 	string _secondName;
 	string _thirdName;
-	Date _dateOfBirth;
-	Human* human;
+	Date** _dateOfBirth;
+	Human** human;
+	int _count;
 };
-
 
 int main()
 {
@@ -91,9 +99,9 @@ int main()
 
 	{
 		Human x;
+		x.CreateHuman();
 		x.ShowHumanInfo();
 	}
 
 	cout << "\nА программа то ещё идёт, а деструктор уже сработал!\n";
 }
-
