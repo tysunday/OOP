7f34b98c9488063e1b961058c66120f5596bb7a6
tysunday <120739860+tysunday@users.noreply.github.com>
2023-04-26 21:41:58 +0500

    Add files via upload

---
+++ test222222222222222/test222222222222222.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+using namespace std;
+
+void HumanObj(Human& object);
+
+class Human {
+private:
+	Human** _human;
+	string _name;
+	int _age;
+public:
+	string getName() { return _name; }
+	int getAge() { return _age; }
+
+	Human() {}
+
+	Human(string name, int age) :
+		_name(name), _age(age) {}
+
+	Human CreateHuman() {
+		int age;
+		string name;
+		int size = 2;
+		*_human = new Human[2];
+
+		for (int i = 0; i < 2; i++) 
+		{
+			cout << "Введите имя: "; cin >> name;
+			cout << "Введите возраст: "; cin >> age;
+			_human[i] = new Human(name, age);
+		}
+	}
+};
+
+int main()
+{
+
+}
+
+
+void HumanObj(Human& object) {
+
+}
